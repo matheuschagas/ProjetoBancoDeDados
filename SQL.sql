594e0577e0aa9511273244039e5bd636a11c357c
Matheus Marrane <matheus.chagas@acad.pucrs.br>
2016-05-13 22:17:48 -0300

    inserts

--- SQL.sql
+++ SQL.sql
@@ -48,6 +48,33 @@ CREATE TABLE Reserva (
   FOREIGN KEY (id_Funcionario) REFERENCES Funcionario (id_Funcionario),
   FOREIGN KEY (id_Equipamento) REFERENCES Equipamento (id_Equipamento)
 );
+
+-- -----------------------------------------------------
+-- Inserção de dados
+-- ----------------------------------------------------
+
+INSERT INTO Funcionario(id_Funcionario, senha, nome_completo, data_nascimento, data_admissao, sexo, endereco1, endereco2, salario_mensal) VALUES(1, '123', 'Douglas Terra', '01/02/1960', '01/02/2006', 'M', 'Rua Paranaue, 738', 'Porto Alegre - RS', 1050.02);
+INSERT INTO Funcionario(id_Funcionario, senha, nome_completo, data_nascimento, data_admissao, sexo, endereco1, endereco2, salario_mensal) VALUES(2, '123', 'Lucas Terra', '01/02/1960', '01/02/2006', 'M', 'Av do Forte, 738', 'Porto Alegre - RS', 1050.02);
+INSERT INTO Funcionario(id_Funcionario, senha, nome_completo, data_nascimento, data_admissao, sexo, endereco1, endereco2, salario_mensal) VALUES(3, '123', 'Fernanda Paes', '01/02/1960', '01/02/2006', 'F', 'Av Ipiranga, 738', 'Porto Alegre - RS', 2050.02);
+INSERT INTO Funcionario(id_Funcionario, senha, nome_completo, data_nascimento, data_admissao, sexo, endereco1, endereco2, salario_mensal) VALUES(4, '123', 'Davi Silva', '01/02/1960', '01/02/2006', 'M', 'Rua das Flores, 738', 'Porto Alegre - RS', 1550.02);
+INSERT INTO Funcionario(id_Funcionario, senha, nome_completo, data_nascimento, data_admissao, sexo, endereco1, endereco2, salario_mensal) VALUES(5, '123', 'Leticia Sintra', '01/02/1960', '01/02/2006', 'F', 'Rua Norberto Rauch, 738', 'Porto Alegre - RS', 1053.02);
+
+INSERT INTO TipoEquipamento(ID_TIPOEQUIPAMENTO, NOME) VALUES(1, 'Furadeiras');
+INSERT INTO TipoEquipamento(ID_TIPOEQUIPAMENTO, NOME) VALUES(2, 'Esmerilhadeiras');
+INSERT INTO TipoEquipamento(ID_TIPOEQUIPAMENTO, NOME) VALUES(3, 'Martelos');
+
+INSERT INTO Equipamento(ID_EQUIPAMENTO, ID_TIPOEQUIPAMENTO, DESCRICAO, DATA_AQUISICAO, CUSTO_DIARIA, EM_MANUTENCAO) VALUES(1, 1, 'Furadeira / Parafusadeira Elétrica 280W 110V', '01/02/2015', 20.50, 1);
+INSERT INTO Equipamento(ID_EQUIPAMENTO, ID_TIPOEQUIPAMENTO, DESCRICAO, DATA_AQUISICAO, CUSTO_DIARIA, EM_MANUTENCAO) VALUES(2, 1, 'Furadeira de Impacto com Mandril de 1/2 Pol 680 W 110 V', '01/02/2015', 20.50, 0);
+INSERT INTO Equipamento(ID_EQUIPAMENTO, ID_TIPOEQUIPAMENTO, DESCRICAO, DATA_AQUISICAO, CUSTO_DIARIA, EM_MANUTENCAO) VALUES(3, 2, 'Esmerilhadeira Angular 4.1/2 Pol. 820 W 220 V', '01/02/2015', 30.50, 0);
+INSERT INTO Equipamento(ID_EQUIPAMENTO, ID_TIPOEQUIPAMENTO, DESCRICAO, DATA_AQUISICAO, CUSTO_DIARIA, EM_MANUTENCAO) VALUES(4, 3, 'Martelo Anti-Retrocesso 1LB', '01/02/2015', 10.50, 0);
+
+INSERT INTO Reserva(ID_RESERVA, ID_FUNCIONARIO, ID_EQUIPAMENTO, DATA_INICIAL, DATA_FINAL) VALUES (1, 1, 2, '10/05/2016', '13/05/2016');
+INSERT INTO Reserva(ID_RESERVA, ID_FUNCIONARIO, ID_EQUIPAMENTO, DATA_INICIAL, DATA_FINAL) VALUES (2, 2, 2, '10/02/2016', '13/02/2016');
+INSERT INTO Reserva(ID_RESERVA, ID_FUNCIONARIO, ID_EQUIPAMENTO, DATA_INICIAL, DATA_FINAL) VALUES (3, 3, 3, '10/05/2016', '13/05/2016');
+INSERT INTO Reserva(ID_RESERVA, ID_FUNCIONARIO, ID_EQUIPAMENTO, DATA_INICIAL, DATA_FINAL) VALUES (4, 2, 2, '10/05/2016', '13/05/2016');
+INSERT INTO Reserva(ID_RESERVA, ID_FUNCIONARIO, ID_EQUIPAMENTO, DATA_INICIAL, DATA_FINAL) VALUES (5, 4, 4, '10/05/2016', '13/05/2016');
+
+
     
     
 -- Consulta 1
